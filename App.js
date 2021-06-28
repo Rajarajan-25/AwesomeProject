d78540815bfd80630602c02cee94fa290ba3c76d
Rajarajan-25 <84223080+Rajarajan-25@users.noreply.github.com>
2021-06-28 10:06:51 +0530

    completed on 25-06-2021

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React,{useRef, useState} from 'react';
+import React,{useRef, useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -12,13 +12,17 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   KeyboardAvoidingView,
-  Alert
+  Alert,
+  Image
 } from 'react-native';
 import { Formik, Field, Form } from 'formik';
 import * as yup from 'yup';
 import DatePicker from 'react-native-datepicker';
 import { date } from 'yup/lib/locale';
 import {Picker} from '@react-native-picker/picker';
+import MultiSelect from 'react-native-multiple-select';
+import icons from './source/common/resources/icons'
+import { relativeTimeRounding } from 'moment';
 
 const loginValidationSchema = yup.object().shape({
   fullName: yup
@@ -53,12 +57,28 @@ const DismissKeyboard = ({children})=>(
   </TouchableWithoutFeedback>
 )
 
+const items = [
+  {id: 1, name: 'Tamil'},
+  {id: 2, name: 'English'},
+  {id: 3, name: 'Telugu'},
+  {id: 4, name: 'Malayalam'},
+  {id: 5, name: 'Kannada'},
+  {id: 6, name: 'Punjabi'},
+  {id: 7, name: 'Bengali'},
+  {id: 8, name: 'Kashmiri'},
+  {id: 9, name: 'Hindi'},
+  {id: 10, name: 'Greek'},
+];
 
 const App = () => {
+  const ref_radioButton = useRef();
+  const ref_dropDown = useRef();
+  const ref_date = useRef();
   const ref_mobileNumber = useRef();
   const ref_email = useRef();
   const ref_password = useRef();
   const ref_confirmPassword = useRef();
+
   
 const [date, setDate]=useState(new Date());
 const [isSelected, setisSelected] = useState([
@@ -77,22 +97,43 @@ const onRadioBtnClick = (item) => {
   setisSelected(updatedState);
 };
 
+const [hidePasswrd,setHidePasswrd]=useState(true);
 
+const managePasswrdVisibility = () => {
+  // console.log(hidePassword);
+  setHidePasswrd((prevState)=> !prevState);
+}
+
+const [hidePassword,setHidePassword]=useState(true);
+
+const managePasswordVisibility = () => {
+  // console.log(hidePassword);
+  setHidePassword((prevState)=> !prevState);
+}
+
+const [selectedItems, setSelectedItems] = useState([]);
+const onSelectedItemsChange = (selectedItems) => {
+  setSelectedItems(selectedItems);
+  };
+
+console.log(hidePassword);
   return (
-    <>
-    
+    <KeyboardAvoidingView behavior='height' style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <ScrollView>
+      
+
+      <ScrollView showsVerticalScrollIndicator={false}>
       <DismissKeyboard>
-      <KeyboardAvoidingView behavior='height' style={styles.container}>
+      
       <View style={styles.loginContainer}>
           <Text>FORMIK FORM WITH YUP VALIDATION</Text>
           <Formik
             validationSchema={loginValidationSchema}
-            initialValues={{ fullName:'',date:'26-01-2021' ,mobileNumber:'' ,email: '', password: '', confirmPassword:'' }}
+            initialValues={{ fullName:'',date:'' ,mobileNumber:'' ,email: '', password: '', confirmPassword:'' }}
             onSubmit={values =>{ 
               values.date=date;
-              Alert.alert(JSON.stringify(values))
+              Alert.alert(JSON.stringify(values));
+              console.log(values);
             
             }}
           >
@@ -106,9 +147,9 @@ const onRadioBtnClick = (item) => {
             isValid }) => (
               <>
                 <TextInput
-                  name="fullName eg) John Smith"
+                  name="fullName"
                   maxLength={20}
-                  placeholder="First and Last Name"
+                  placeholder="First and Last Name (e.g. John Smith)"
                   style={styles.textInput}
                   onChangeText={handleChange('fullName')}
                   onBlur={handleBlur('fullName')}
@@ -116,7 +157,7 @@ const onRadioBtnClick = (item) => {
                   keyboardType="default"
                   autoFocus={true}
                   returnKeyType="next"
-                  onSubmitEditing={() => ref_mobileNumber.current.focus()}
+                  onSubmitEditing={() => ref_radioButton.current.focus()}
                 />
                 {errors.fullName &&
                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.fullName}</Text>
@@ -129,6 +170,8 @@ const onRadioBtnClick = (item) => {
               {isSelected.map((item) => (
               <View style={styles.radioButtonContainer}>
                 <TouchableOpacity
+                onSubmitEditing={() => ref_dropDown.current.focus()}
+                ref={ref_radioButton}
                   onPress={() => {
                     onRadioBtnClick(item);
                   }}
@@ -148,26 +191,56 @@ const onRadioBtnClick = (item) => {
           </View>
 
           <View style={styles.ageData}>
-            <Text>Language</Text>
+            <Text>Time Zone</Text>
           
           <View style={styles.pickDrop}>
             <Picker
+              onSubmitEditing={() => ref_date.current.focus()}
+              ref={ref_dropDown}
               selectedValue={selectedZone}
               onValueChange={(itemValue, itemIndex) =>
                 setSelectedZone(itemValue)
               }>
-              <Picker.Item label="Tamil" value="tamil" />
-              <Picker.Item label="Telugu" value="telugu" />
-              <Picker.Item label="Malayalam" value="malayalam" />
-              <Picker.Item label="Kannada" value="kannada" />
-              <Picker.Item label="Marathi" value="marathi" />
-              <Picker.Item label="Hindi" value="hindi" />
+              <Picker.Item label="Greenwich Mean Time" value="GMT" />
+              <Picker.Item label="European Central Time" value="GMT+1:00" />
+              <Picker.Item label="Middle East Time" value="GMT+3:30" />
+              <Picker.Item label="Indian Standard Time" value="GMT+5:30" />
+              <Picker.Item label="Austaralia Central Time" value="GMT+9:30" />
+              <Picker.Item label="Cental African Time" value="GMT-1:00" />
             </Picker>
           </View>
           </View>
+          <View style={{width:'100%', marginTop:'4%', marginBottom:'4%'}}>
+          <MultiSelect
+          fixedHeight={false}
+          hideTags
+          styleDropdownMenu={{borderRadius:10, width:'99%' ,alignSelf:'center', justifyContent:'center'}}
+          tagBorderColor='black'
+          items={items}
+          uniqueKey="id"
+          onSelectedItemsChange={onSelectedItemsChange}
+          selectedItems={selectedItems}
+          selectText="    Pick Languages"
+          searchInputPlaceholderText="Search Languages..."
+          onChangeInput={(text) => console.log(text)}
+          tagRemoveIconColor="#CCC"
+          tagBorderColor="#CCC"
+          tagTextColor="#CCC"
+          selectedItemTextColor="#CCC"
+          selectedItemIconColor="#CCC"
+          itemTextColor="#000"
+          displayKey="name"
+          searchInputStyle={{color: '#CCC'}}
+          submitButtonColor="#bfe3aa"
+          submitButtonText="Submit"
+          hideSubmitButton={true}
+        />
+        </View>
+
             
             <DatePicker
-              
+              onSubmitEditing={() => ref_mobileNumber.current.focus()}
+              ref={ref_date}
               style={styles.textInput}
                date={date} 
               onDateChange={setDate}
@@ -176,7 +249,7 @@ const onRadioBtnClick = (item) => {
               placeholder="select date"
               format="DD-MM-YYYY"
               minDate="01-01-1921"
-              maxDate="24-06-2021"
+              maxDate= {new Date()}
               confirmBtnText="Confirm"
               cancelBtnText="Cancel"
               customStyles={{
@@ -204,7 +277,7 @@ const onRadioBtnClick = (item) => {
 
                 <TextInput
                   name="mobileNumber"
-                  placeholder="Mobile Number"
+                  placeholder="Mobile Number   (e.g.) 919090909090"
                   style={styles.textInput}
                   onChangeText={handleChange('mobileNumber')}
                   onBlur={handleBlur('mobileNumber')}
@@ -214,7 +287,7 @@ const onRadioBtnClick = (item) => {
                   onSubmitEditing={() => ref_email.current.focus()}
                   ref={ref_mobileNumber}
                 />
-                 {errors.email &&
+                 {errors.mobileNumber &&
                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.mobileNumber}</Text>
                  }
 
@@ -222,7 +295,7 @@ const onRadioBtnClick = (item) => {
                 <TextInput
                   name="email"
                   maxLength={30}
-                  placeholder="Email Address"
+                  placeholder="Email Address (e.g.) smith@mail.com"
                   style={styles.textInput}
                   onChangeText={handleChange('email')}
                   onBlur={handleBlur('email')}
@@ -237,36 +310,59 @@ const onRadioBtnClick = (item) => {
                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
                  }
 
-                
+                <View style={styles.passwrdInput}>    
                 <TextInput
                   name="password"
-                  placeholder="Password"
-                  maxLength={15}
-                  style={styles.textInput}
+                  style={styles.passwordArea}
+                  placeholder="Password (at least 8 characters)"
+                  maxLength={20}
                   onChangeText={handleChange('password')}
                   onBlur={handleBlur('password')}
                   value={values.password}
                   autoFocus={true}
                   onSubmitEditing={() => ref_confirmPassword.current.focus()}
                   ref={ref_password}
-                  secureTextEntry
+                  secureTextEntry={hidePasswrd}
                 />
+                <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.visibilityBtn}
+                onPress={managePasswrdVisibility}
+                ><Image 
+                source={(hidePasswrd)?
+                require('./source/common/resources/icons/EyeClose.png'):
+                require('./source/common/resources/icons/EyeOpen.png')}
+                style={styles.btnImage}
+                  /></TouchableOpacity>
+                </View>
                 {errors.password &&
                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
                 }
 
+                <View style={styles.passwrdInput}>
                 <TextInput
                   name="confirmPassword"
                   maxLength={20}
                   placeholder="Confirm Password"
-                  style={styles.textInput}
+                  style={styles.passwordArea}
                   onChangeText={handleChange('confirmPassword')}
                   onBlur={handleBlur('confirmPassword')}
                   value={values.confirmPassword}
-                  secureTextEntry
+                  secureTextEntry={hidePassword}
                   autoFocus={true}
                   ref={ref_confirmPassword}
                 />
+                <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.visibilityBtn}
+                onPress={managePasswordVisibility}
+                ><Image 
+                source={(hidePassword)?
+                require('./source/common/resources/icons/EyeClose.png'):
+                require('./source/common/resources/icons/EyeOpen.png')}
+                style={styles.btnImage}
+                  /></TouchableOpacity>
+                </View>
                 {errors.confirmPassword &&
                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.confirmPassword}</Text>
                 }
@@ -277,27 +373,27 @@ const onRadioBtnClick = (item) => {
             )}
           </Formik>
         </View>
-      </KeyboardAvoidingView>
+      
       </DismissKeyboard>
       </ScrollView>
-    </>
+      </KeyboardAvoidingView>
+    
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width:'100%',
     justifyContent: 'center',
-    alignItems: 'center',
-    marginTop:30
+    alignItems:'center',
+    marginTop:15,
+    marginBottom:15
   },
+
   loginContainer: {
-    
-    width: '80%',
-    alignItems: 'center',
-  },
-  loginContainer: {
-    width: '80%',
+    width: '94%',
+    alignSelf:'center',
     alignItems: 'center',
     backgroundColor: 'white',
     padding: 10,
@@ -305,6 +401,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#e6e6e6'
   },
   textInput: {
+    paddingLeft:'5%',
+    height: 40,
+    width: '100%',
+    margin: 10,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+    marginVertical:'4%'
+  },
+  passwordArea: {
+    height:'100%',
+    width:'100%'
+  },
+  passwrdInput: {
+    flexDirection:'row',
     paddingLeft:'5%',
     height: 40,
     width: '100%',
@@ -379,8 +491,20 @@ const styles = StyleSheet.create({
     position: 'relative',
     marginLeft: '7%',
     justifyContent: 'center',
-  }
-
+  },
+  visibilityBtn: { 
+    position: 'absolute', 
+    right: 3, 
+    height: 40, 
+    width: 35, 
+    padding: 5 }, 
+        
+    btnImage: { 
+    resizeMode: 'contain', 
+    height: '80%', 
+    width: '80%' } 
+      
+  
 })
 
 
